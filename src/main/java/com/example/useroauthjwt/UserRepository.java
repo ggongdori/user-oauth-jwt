2cc81f03b94a2015e54b2aad1076ed9f9d4c1142
ggongdori <soominlee0410@gmail.com>
2022-05-10 21:56:31 +0900

    userrepository impl

--- src/main/java/com/example/useroauthjwt/UserRepository.java
+++ src/main/java/com/example/useroauthjwt/UserRepository.java
@@ -2,5 +2,11 @@ package com.example.useroauthjwt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
+    boolean existsByNickname(String nickname);
+    boolean existsByEmail(String email);
 }
